5d103d55dd6c66cbcc1a4044f1cfa1e59d021f71
hongnhung20 <hongnhungtdm0905@gmail.com>
2024-12-25 23:48:11 +0700

    done api register

--- client/src/containers/Public/Login.js
+++ client/src/containers/Public/Login.js
@@ -1,31 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { InputForm, Button } from "../../components";
-
+import { useLocation } from "react-router-dom";
+import { apiRegister } from "../../services/auth";
+import * as actions from "../../store/actions";
+import { useDispatch } from "react-redux";
 const Login = () => {
-  const make = "Ford";
-  const model = "Mustang";
-  const car = { make, model };
-  console.log(car);
+  const location = useLocation();
+  const dispath = useDispatch();
+  const [isRegister, setIsRegister] = useState(location.state?.flag);
+  const [payload, setPayload] = useState({ phone: "", password: "", name: "" });
+
+  useEffect(() => {
+    setIsRegister(location.state?.flag);
+  }, [location.state?.flag]);
+
+  const handleSubmit = async () => {
+    console.log(payload);
+    dispath(actions.register(payload));
+    // console.log(response);
+  };
   return (
     <div className="bg-white  w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm ">
-      <h3 className="font-semibold text-2xl mb-5">Đăng nhập</h3>
+      <h3 className="font-semibold text-2xl mb-5">
+        {isRegister ? "Tạo tài khoản mới" : "Đăng nhập"}
+      </h3>
       <div className="w-full flex flex-col gap-7">
-        <InputForm label={"SỐ ĐIỆN THOẠI"} />
-        <InputForm label={"MẬT KHẨU"} />
+        {isRegister && (
+          <InputForm
+            label={"HỌ TÊN"}
+            value={payload.name}
+            setValue={setPayload}
+            type="name"
+          />
+        )}
+        <InputForm
+          label={"SỐ ĐIỆN THOẠI"}
+          value={payload.phone}
+          setValue={setPayload}
+          type="phone"
+        />
+        <InputForm
+          label={"MẬT KHẨU"}
+          value={payload.password}
+          setValue={setPayload}
+          type="password"
+        />
         <Button
-          text={"Đăng nhập"}
+          text={isRegister ? "Đăng Ký" : "Đăng nhập"}
           bgColor={"bg-secondary1"}
           textColor={"text-white"}
           fullwidth
+          onClick={handleSubmit}
         />
       </div>
       <div className="mt-7 flex items-center justify-between">
-        <small className="text-[blue] hover:text-[red] cursor-pointer">
-          Bạn quên mật khẩu{" "}
-        </small>
-        <small className="text-[blue] hover:text-[red] cursor-pointer">
-          Tạo tài khoản mới
-        </small>
+        {isRegister ? (
+          <small>
+            Bạn đã có tài khoản{" "}
+            <span
+              onClick={() => {
+                setIsRegister(false);
+              }}
+              className="text-blue-500 hover:underline cursor-pointer"
+            >
+              Đăng nhập ngay
+            </span>
+          </small>
+        ) : (
+          <>
+            <small className="text-[blue] hover:text-[red] cursor-pointer">
+              Bạn quên mật khẩu{" "}
+            </small>
+            <small
+              className="text-[blue] hover:text-[red] cursor-pointer"
+              onClick={() => {
+                setIsRegister(true);
+              }}
+            >
+              Tạo tài khoản mới
+            </small>
+          </>
+        )}
       </div>
     </div>
   );
--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,11 +1,27 @@
+import actionTypes from "../actions/actionTypes";
+
 const initState = {
-    isLoggedIn : false,
-    token: null
-}
-const userReducer = (state = initState,acction)=>{
-    switch(acction.type){
-        default:
-            return state;
-    }
-}
-export default userReducer
+  isLoggedIn: false,
+  token: null,
+  msg: "",
+};
+const userReducer = (state = initState, acction) => {
+  switch (acction.type) {
+    case actionTypes.REGISTER_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+        token: acction.data,
+      };
+    case actionTypes.REGISTER_FAIL:
+      return {
+        ...state,
+        isLoggedIn: false,
+        msg: acction.data,
+        token: null,
+      };
+    default:
+      return state;
+  }
+};
+export default userReducer;
